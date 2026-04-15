b806d2667c294a3953044c04b2ce99990541c32a
Pedro Armando <pedroarmandodev@gmail.com>
2026-04-15 14:29:50 -0300

    correcao pequena

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
-require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
+require("dotenv").config({
+  path: require("path").resolve(__dirname, "../.env"),
+});
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
